363293ba6e702364028c5ad2041046006918c48a
ladyada <limor@ladyada.net>
2023-09-03 19:23:10 -0400

    doxyclang

--- Adafruit_AD569x.cpp
+++ Adafruit_AD569x.cpp
@@ -10,7 +10,7 @@
  * Adafruit AD5693 breakout: https://www.adafruit.com/product/XXXX
  *
  * @section dependencies Dependencies
- * 
+ *
  * This library depends on the Adafruit BusIO library.
  *
  * @section author Author
@@ -32,59 +32,60 @@ Adafruit_AD569x::Adafruit_AD569x() {
   i2c_dev = nullptr; // Initialize the I2C device pointer to nullptr
 }
 
-
 /**
  * @brief Initialize the AD569x chip for communication.
- * 
+ *
  * This function initializes the I2C interface and sets up the AD569x chip
  * for communication. It should be called in the setup() function of your
  * Arduino sketch. Will perform a soft reset and configure for normal mode,
  * with Vref on, and 1x gain output.
- * 
+ *
  * @param addr The I2C address of the AD569x chip, 0x4C default or 0x4E
  * @param wire The I2C interface to use (usually &Wire).
  * @return true if the I2C device was found, false otherwise.
  */
 bool Adafruit_AD569x::begin(uint8_t addr, TwoWire *wire) {
-  if (i2c_dev) delete i2c_dev;
+  if (i2c_dev)
+    delete i2c_dev;
 
   // Create and initialize the I2C device object
   i2c_dev = new Adafruit_I2CDevice(addr, wire);
 
   // Try to initialize the device
-  if (! i2c_dev->begin() ) 
+  if (!i2c_dev->begin())
     return false;
 
   reset();
-  
+
   if (!setMode(NORMAL_MODE, true /* enable ref */, false /* gain2x */))
     return false;
 
   return true;
 }
 
-
 /**
  * @brief Set the operating mode, reference, and gain for the AD569x chip.
- * 
+ *
  * This function writes to the control register of the AD569x chip to set
  * the operating mode, enable or disable the reference, and set the gain.
- * 
- * @param newmode The new operating mode to set (from ad569x_operating_modes enum).
- * @param enable_ref Whether to enable the reference (true) or disable it (false).
+ *
+ * @param newmode The new operating mode to set (from ad569x_operating_modes
+ * enum).
+ * @param enable_ref Whether to enable the reference (true) or disable it
+ * (false).
  * @param gain2x Whether to set the gain to 2xVref (true) or 1x (false).
  * @return true if the I2C write operation was successful
  */
-bool Adafruit_AD569x::setMode(ad569x_operating_modes newmode, 
-                              bool enable_ref, bool gain2x) {
+bool Adafruit_AD569x::setMode(ad569x_operating_modes newmode, bool enable_ref,
+                              bool gain2x) {
   // Prepare the command byte
   uint8_t command = ad569x_commands::WRITE_CONTROL;
 
   // Prepare the high and low data bytes
   uint16_t data = 0x0000;
-  data |= ((uint16_t)newmode << 13);  // Set D14 and D13 for the operating mode
+  data |= ((uint16_t)newmode << 13); // Set D14 and D13 for the operating mode
   data |= ((uint16_t)!enable_ref << 12); // Set D12 for enable_ref
-  data |= ((uint16_t)gain2x << 11);  // Set D11 for the gain
+  data |= ((uint16_t)gain2x << 11);      // Set D11 for the gain
 
   uint8_t highByte = (data >> 8) & 0xFF;
   uint8_t lowByte = data & 0xFF;
@@ -96,15 +97,17 @@ bool Adafruit_AD569x::setMode(ad569x_operating_modes newmode,
   return i2c_dev->write(buffer, 3);
 }
 
-
 /**
- * @brief Write a 16-bit value to the input register and update the DAC register.
- * 
+ * @brief Write a 16-bit value to the input register and update the DAC
+ * register.
+ *
  * This function writes a 16-bit value to the input register and then updates
  * the DAC register of the AD569x chip in a single operation
- * 
- * @param value The 16-bit value to write to the input register and update the DAC register.
- * @return true if the write and update operation was successful, false otherwise.
+ *
+ * @param value The 16-bit value to write to the input register and update the
+ * DAC register.
+ * @return true if the write and update operation was successful, false
+ * otherwise.
  */
 bool Adafruit_AD569x::writeUpdateDAC(uint16_t value) {
   // Prepare the command byte
@@ -121,13 +124,12 @@ bool Adafruit_AD569x::writeUpdateDAC(uint16_t value) {
   return i2c_dev->write(buffer, 3);
 }
 
-
 /**
  * @brief Write a 16-bit value to the DAC register... does NOT output it!
- * 
+ *
  * This function writes a 16-bit value to the input register of the AD569x chip.
  * The data does not appear on the output of the DAC till you run updateDAC()!
- * 
+ *
  * @param value The 16-bit value to write to the DAC input register.
  * @return true if the write operation was successful, false otherwise.
  */
@@ -147,18 +149,18 @@ bool Adafruit_AD569x::writeDAC(uint16_t value) {
   return i2c_dev->write(buffer, 3);
 }
 
-
 /**
  * @brief Update the DAC register from the input register.
- * 
+ *
  * This function sends the UPDATE_DAC command to the AD569x chip to update
  * the DAC register based on the value stored in the input register.
- * 
+ *
  * @return true if the update operation was successful, false otherwise.
  */
 bool Adafruit_AD569x::updateDAC(void) {
   // Prepare the command byte
-  uint8_t command = ad569x_commands::UPDATE_DAC; // Assuming you've defined the enum as ad569x_commands
+  uint8_t command = ad569x_commands::UPDATE_DAC; // Assuming you've defined the
+                                                 // enum as ad569x_commands
 
   // Prepare a 3-byte buffer: command byte followed by two 0x00 bytes
   uint8_t buffer[3] = {command, 0x00, 0x00};
@@ -167,17 +169,18 @@ bool Adafruit_AD569x::updateDAC(void) {
   return i2c_dev->write(buffer, 3);
 }
 
-
 /**
  * @brief Soft-reset the AD569x chip.
- * 
+ *
  * This function writes 0x8000 to the control register of the AD569x chip
  * to perform a reset operation. Resets the DAC to zero-scale and
  * resets the input, DAC, and control registers to their default values.
  */
 void Adafruit_AD569x::reset(void) {
   // Prepare the command byte
-  uint8_t command = ad569x_commands::WRITE_CONTROL; // Assuming you've defined the enum as ad569x_commands
+  uint8_t command =
+      ad569x_commands::WRITE_CONTROL; // Assuming you've defined the enum as
+                                      // ad569x_commands
 
   // Prepare the high and low data bytes for 0x8000
   // Combine the command and data into a single 3-byte buffer
